36c30e160380f988d4ebc6d96aa46289a0269105
DurrYCheng <lamert93389744@gmail.com>
2022-10-05 10:13:03 +0800

    done

--- 109360451_楊澄/2.32/source/Main.c
+++ 109360451_楊澄/2.32/source/Main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 int main(void)
 {
-	printf("*********       ***              *              *\n");
-	printf("*       *     *     *           ***            * *\n");
-	printf("*       *    *       *         *****          *   *\n");
-	printf("*       *    *       *           *           *     *\n");
-	printf("*       *    *       *           *          *       *\n");
-	printf("*       *    *       *           *           *     *\n");
-	printf("*       *    *       *           *            *   *\n");
-	printf("*       *     *     *            *             * *\n");
-	printf("*********       ***              *              *\n");
-	//space for 1,\t for 8.
+	float height, weight, bmi;
+	printf("Enter your height(meters) and weight(kilograms): \n");
+	scanf("%f%f", &height, &weight);
+	printf("h=%.2f\nw=%.2f\n", height, weight);
+	bmi = (weight) / (pow(height,2));
+	printf("Your BMI is: %.2f\n", bmi);
+	if (bmi < 18.5)
+	{
+		printf("Underweight\n");
+	}
+	else if (bmi > 18.5 && bmi < 24.9)
+	{
+		printf("Normal\n");
+	}
+	else if (bmi > 25 && bmi < 29.9)
+	{
+		printf("Overwight\n");
+	}
+	else
+	{
+		printf("Obese\n");
+	}
 	system("pause");
 	return 0;
 }
--- 109360451_楊澄/2.33/source/Main.c
+++ 109360451_楊澄/2.33/source/Main.c
@@ -3,16 +3,21 @@
 
 int main(void)
 {
-	printf("*********       ***              *              *\n");
-	printf("*       *     *     *           ***            * *\n");
-	printf("*       *    *       *         *****          *   *\n");
-	printf("*       *    *       *           *           *     *\n");
-	printf("*       *    *       *           *          *       *\n");
-	printf("*       *    *       *           *           *     *\n");
-	printf("*       *    *       *           *            *   *\n");
-	printf("*       *     *     *            *             * *\n");
-	printf("*********       ***              *              *\n");
-	//space for 1,\t for 8.
+	float miles, gas, MperG, park, toll ,cost ,a;
+	printf("Total miles driven per day:\n");
+	scanf("%f", &miles);
+	printf("Cost per gallon of gasoline:\n");
+	scanf("%f",&gas );
+	printf("Average miles per gallon:\n");
+	scanf("%f",&MperG );
+	printf("Parking fees per day:\n");
+	scanf("%f",&park );
+	printf("Tolls per day:\n");
+	scanf("%f",&toll );
+	//printf("%.2f%.2f%.2f%.2f%.2f", miles, gas, MperG, park, toll);
+	a = miles * gas / MperG;
+	cost = a + park + toll;
+	printf("Your cost is: %.2f $\n",cost);
 	system("pause");
 	return 0;
 }
